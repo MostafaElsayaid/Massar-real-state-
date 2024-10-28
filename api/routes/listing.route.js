147002f6d40438ee688909b05d4a16defb1c375f
MostafaElsayaid <sosta12345sosta@gmail.com>
2024-10-28 02:59:10 +0200

    ceate search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { createlisting,deleteListing,updateListing, getListing } from "../controllers/listing.controller.js";
+import { createlisting,deleteListing,updateListing, getListing, getListings} from "../controllers/listing.controller.js";
 import { verfiyToken } from "../utils/verfiyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', verfiyToken,createlisting)
 router.delete('/delete/:id', verfiyToken, deleteListing)
 router.post('/update/:id', verfiyToken, updateListing)
 router.get('/get/:id',  getListing)
+router.get('/get',getListings)
 
 export default router;
